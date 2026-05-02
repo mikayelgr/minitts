62fd5b83ee651e194215eebe141226de28bb9675
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2026-05-03 03:11:18 +0400

    Update tests

--- api/app/test_main.py
+++ api/app/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.text == "OK"
+    assert response.json() == {"status": "ok"}
