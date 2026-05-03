d737390233ff321d2087460fdba819ad004bb315
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2026-05-03 14:58:38 +0400

    Update Docker compose and development workflow

--- scripts/up-devel.sh
+++ scripts/up-devel.sh
@@ -3,3 +3,7 @@
 docker compose run -dp 5672:5672 rabbitmq
 docker compose run -dp 5432:5432 postgres
 docker compose run -dp 6379:6379 redis
+
+# Core services
+docker compose run --build -dp 8080:8080 api
+docker compose run --build -dp 8081:8081 soprano
